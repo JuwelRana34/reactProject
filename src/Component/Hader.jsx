d7e634529c15d058bacb43e5893f8de3330807a2
MD. Juwel Rana <rk370613@gmail.com>
2024-05-23 19:03:00 +0600

    done

--- src/Component/Hader.jsx
+++ src/Component/Hader.jsx
@@ -23,7 +23,7 @@ function Hader() {
        <Link   to="/Contact"><button  className="contactbtn"> Contact</button> </Link>
      
        <button  className="downloadbtn" >
-         <FileDownload fileName="Download Cv" fileUrl=".././../public/Juwel Rana's CV.pdf"/>
+         <FileDownload fileName="Download_Cv.pdf" fileUrl="../../public/Juwel Rana's CV.pdf"/>
          <FaDownload className=" ml-2 text-xl" /> 
        </button>
 
