e7635d1ba701177696a22502032dcce03ee86d1b
MD. Juwel Rana <rk370613@gmail.com>
2024-06-29 23:34:30 +0600

    #

--- src/Pages/Nopage.jsx
+++ src/Pages/Nopage.jsx
@@ -7,16 +7,6 @@ import { Button, Empty } from 'keep-react'
 function Nopage() {
   return (
     <>
-    {/* <div className=" w-full h-auto mx-auto  justify-center items-center">
-       <img src={Image} alt="" className=" w-[95%] h-auto justify-center items-center"  />
-       <button className=" p-4 m-1 justify-center items-center mx-auto bg-blue-500 text-center text-white text-lg font-serif rounded-md ">
-       <Link to='/' >  Back</Link>
-        
-       </button>
-
-    </div> */}
-
-
 
     <Empty>
       <Empty.Image>
